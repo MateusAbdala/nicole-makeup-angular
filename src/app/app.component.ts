be5ba02122467aa6991ded42362b31591f00cf37
mateus.abdala <mateus.abdala@unicred.com.br>
2019-01-18 15:26:23 -0200

    beautyServices mockedData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-declare var $ :any;
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -11,16 +11,17 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     // set ScrollSpy for Animations
     $(window).scroll(function () {
-			if ($(document).scrollTop() > 50) {
-				$('nav').addClass('shrink');
-			} else {
-				$('nav').removeClass('shrink');
-			}
-		});
+      if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+      } else {
+        $('nav').removeClass('shrink');
+      }
+    });
     // set Mock
     localStorage.setItem('gallery', JSON.stringify(this.gallery));
     localStorage.setItem('carousel', JSON.stringify(this.carousel));
     localStorage.setItem('institutionalData', JSON.stringify(this.institutionalData));
+    localStorage.setItem('beautyServices', JSON.stringify(this.beautyServices));
   }
 
   // TODO: change origin to an API
@@ -129,5 +130,44 @@ export class AppComponent implements OnInit {
       longitude: ''
     }
   }
+
+  makeupServices = [
+    {
+      title: 'Maquiagem Artística',
+      price: '~~',
+      keywords: ['Duração variada', 'Caracterização', 'Transformação', 'Efeitos Especiais'],
+      description: 'loren ipsum'
+    },
+    {
+      title: 'Maquiagem Profissional',
+      price: '~~',
+      keywords: ['Duração variada', 'SunStripping', 'Cerimônias', 'Makeover'],
+      description: 'loren ipsum'
+    },
+  ];
+  eyebrowServices = [
+    {
+      title: 'Design Tradicional',
+      price: '60',
+      keywords: ['Duração média de 30min', 'Eyebrow Design', 'Shaping', 'Extreme Care'],
+      description: 'loren ipsum'
+    },
+    {
+      title: 'Design com Henna',
+      price: '80',
+      keywords: ['Duração média de 1 hora', 'Eyebrow Makeover', 'Henna', 'Shaping'],
+      description: 'loren ipsum'
+    },
+  ];
+  depilationServices = [
+    {
+      title: 'Depilação Egípcia Facial',
+      price: '30',
+      keywords: ['Duração média de 30min', 'Linhas Soft', 'Evita Pelos Encravados', 'Não Causa Alergias'],
+      description: 'loren ipsum'
+    },
+  ];
+
+  beautyServices = [this.makeupServices, this.eyebrowServices, this.depilationServices];
 
 }
